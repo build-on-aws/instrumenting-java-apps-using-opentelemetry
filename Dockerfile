fb8ff747e738c497a52fe5d57261e9e03aa4f116
Ricardo Ferreira <riferrei@riferrei.com>
2022-09-16 13:34:19 -0400

    First commit

--- Dockerfile
+++
@@ -1,9 +0,0 @@
-FROM maven:3.8.1-openjdk-17-slim
-
-VOLUME /tmp
-ADD . /usr/src/app
-WORKDIR /usr/src/app
-
-RUN mvn clean package -DskipTests
-RUN curl -L https://github.com/aws-observability/aws-otel-java-instrumentation/releases/download/v1.17.0/aws-opentelemetry-agent.jar --output opentelemetry-javaagent-all.jar
-ENTRYPOINT [ "java", "-javaagent:opentelemetry-javaagent-all.jar", "-jar", "target/hello-app-1.0.jar" ]
--- src/main/java/tutorial/buildon/aws/o11y/HelloAppController.java
+++ src/main/java/tutorial/buildon/aws/o11y/HelloAppController.java
@@ -2,21 +2,7 @@ package tutorial.buildon.aws.o11y;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.*;
-
-import io.opentelemetry.api.GlobalOpenTelemetry;
-import io.opentelemetry.api.metrics.LongCounter;
-import io.opentelemetry.api.metrics.Meter;
-import io.opentelemetry.api.trace.Span;
-import io.opentelemetry.api.trace.Tracer;
-import io.opentelemetry.context.Scope;
-import io.opentelemetry.instrumentation.annotations.WithSpan;
-
-import static tutorial.buildon.aws.o11y.Constants.*;
-import static java.lang.Runtime.*;
-
-import javax.annotation.PostConstruct;
 
 @RestController
 public class HelloAppController {
@@ -24,62 +10,15 @@ public class HelloAppController {
     private static final Logger log =
         LoggerFactory.getLogger(HelloAppController.class);
 
-    @Value("otel.traces.api.version")
-    private String tracesApiVersion;
-
-    @Value("otel.metrics.api.version")
-    private String metricsApiVersion;
-
-    private final Tracer tracer =
-        GlobalOpenTelemetry.getTracer("io.opentelemetry.traces.hello",
-            tracesApiVersion);
-
-    private final Meter meter =
-        GlobalOpenTelemetry.meterBuilder("io.opentelemetry.metrics.hello")
-            .setInstrumentationVersion(metricsApiVersion)
-            .build();
-
-    private LongCounter numberOfExecutions;
-
-    @PostConstruct
-    public void createMetrics() {
-
-        numberOfExecutions =
-            meter
-                .counterBuilder(NUMBER_OF_EXEC_NAME)
-                .setDescription(NUMBER_OF_EXEC_DESCRIPTION)
-                .setUnit("int")
-                .build();
-
-        meter
-            .gaugeBuilder(HEAP_MEMORY_NAME)
-            .setDescription(HEAP_MEMORY_DESCRIPTION)
-            .setUnit("byte")
-            .buildWithCallback(
-                r -> {
-                    r.record(getRuntime().totalMemory() - getRuntime().freeMemory());
-                });
-
-    }
-
     @RequestMapping(method= RequestMethod.GET, value="/hello")
     public Response hello() {
         Response response = buildResponse();
-        // Creating a custom span
-        Span span = tracer.spanBuilder("mySpan").startSpan();
-        try (Scope scope = span.makeCurrent()) {
-            if (response.isValid()) {
-                log.info("The response is valid.");
-            }
-            // Update the synchronous metric
-            numberOfExecutions.add(1);
-        } finally {
-            span.end();
+        if (response.isValid()) {
+            log.info("The response is valid.");
         }
         return response;
     }
 
-    @WithSpan
     private Response buildResponse() {
         return new Response("Hello World");
     }
